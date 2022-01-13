7984fa6c72ae647c100d37aa2e07b69586c29515
Vijay Pandey <vijay@ith.tech>
2022-01-13 16:09:28 +0530

    gitignore added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import MainRouter from "./App.routing";
 import { Layout } from "antd";
--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ ReactDOM.render(
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
            <App />
-      </PersistGate>
+    </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
